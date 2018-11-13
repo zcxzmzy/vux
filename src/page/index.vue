68a1abed64ad240224d0746959adbeae977cf602
zhaochengxian <zhaochengxian@soyoung.com>
2018-11-13 17:16:28 +0800

    版本1.0

---
+++ src/page/index.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+    div.index
+       div 首页
+</template>
+<script>
+    import mixin from '../mixins/mixinLogin'
+    export default {
+        name: "index",
+        created(){
+        },
+        methods:{
+
+        }
+
+    }
+</script>
+
+<style lang="less">
+    .index {
+
+    }
+
+
+</style>
